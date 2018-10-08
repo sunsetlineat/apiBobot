bf504ee78c6103b69c6effbc7b0cb2705f6db6b6
Aphisit Sipalat <ti.bitkub@gmail.com>
2018-10-08 13:50:01 +0700

    ำกด

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use pimax\FbBotApp;
+use pimax\Messages\Message;
 use App\Http\Requests;
 
 class MessengerController extends Controller
@@ -23,13 +24,14 @@ class MessengerController extends Controller
     		'message'		=>	['text' => 'Thanks for watching! :)']
     	];
 
-    	$this->sendMessage($response);
+		$this->sendMessage($response);
+		return "This is the webhook";
     }
 
     protected function sendMessage($response)
     {
     	// set our post
-    	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env('PAGE_ACCESS_TOKEN'));
+    	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env('ACCESS_TOKEN'));
     	curl_setopt($ch, CURLOPT_POST, 1);
     	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
     	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
@@ -42,13 +44,13 @@ class MessengerController extends Controller
     	// FACEBOOK_MESSENGER_WEBHOOK_TOKEN is not exist yet.
     	// we can set that up in our .env file
     	$local_token = env('VERIFY_TOKEN');
-    	$hub_verify_token = request('hub_verify_token');
+    	$hub_verify_token = \Input::get('hub_verify_token');
 
     	// condition if our local token is equal to hub_verify_token
     	if ($hub_verify_token === $local_token) {
     		// echo the hub_challenge in able to verify.
-    		echo request('hub_challenge');
-    		exit;
+    		return \Input::get('hub_challenge');
+    		
     	}
     }
 }
